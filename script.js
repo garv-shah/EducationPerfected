383ac010d4b07ecfc7c7bb191ae57440794ee201
Restyled.io <commits@restyled.io>
2021-10-15 04:37:22 +0000

    Restyled by prettier

--- script.js
+++ script.js
@@ -12,7 +12,7 @@
 // @grant        none
 // ==/UserScript==
 
-(async function() {
+(async function () {
   "use strict";
   let semiTOGGLE = false;
   let autoTOGGLE = false;
@@ -21,7 +21,9 @@
   let answerSub = "";
   let msg = "";
 
-  function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
+  function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
 
   function cutString(string) {
     string = String(string.replace(/ *\([^)]*\) */g, "").split("; ")[0]);
@@ -31,8 +33,9 @@
 
   function wordList(className) {
     let words = [];
-    document.querySelectorAll("div." + className)
-        .forEach(i => words.push(i.innerText));
+    document
+      .querySelectorAll("div." + className)
+      .forEach((i) => words.push(i.innerText));
     return words;
   }
 
@@ -47,40 +50,45 @@
 
   async function submitButton() {
     let explanation = document.querySelector("button#explanation-button");
-    if (explanation)
-      return explanation;
+    if (explanation) return explanation;
     console.log("didn't find hax button");
     return document.querySelector("button.submit-button");
   }
 
   function deleteModals() {
-    document.querySelectorAll("div[uib-modal-window=modal-window]")
-        .forEach(i => i.remove());
-    document.querySelectorAll("div[uib-modal-backdrop=modal-backdrop]")
-        .forEach(i => i.remove());
+    document
+      .querySelectorAll("div[uib-modal-window=modal-window]")
+      .forEach((i) => i.remove());
+    document
+      .querySelectorAll("div[uib-modal-backdrop=modal-backdrop]")
+      .forEach((i) => i.remove());
   }
 
   function findAnswer(question) {
     let answer = fullDict[question];
-    if (answer === undefined)
-      answer = fullDict[question.replace(",", ";")];
-    if (answer === undefined)
-      answer = cutDict[cutString(question)];
-    return answer
+    if (answer === undefined) answer = fullDict[question.replace(",", ";")];
+    if (answer === undefined) answer = cutDict[cutString(question)];
+    return answer;
   }
 
   async function correctAnswer(question) {
     msg = msg + "Thought question: \n" + question + "\n";
     for (let i = 0; i < 300; i++) {
-      if (cutString(document.querySelector("td#question-field").innerText) ===
-          cutString(question))
+      if (
+        cutString(document.querySelector("td#question-field").innerText) ===
+        cutString(question)
+      )
         break;
       await sleep(10);
     }
-    msg = msg + "Modal question: " +
-          document.querySelector("td#question-field").innerText + "\n";
-    let answer =
-        cutString(document.querySelector("td#correct-answer-field").innerText);
+    msg =
+      msg +
+      "Modal question: " +
+      document.querySelector("td#question-field").innerText +
+      "\n";
+    let answer = cutString(
+      document.querySelector("td#correct-answer-field").innerText
+    );
     document.querySelector("button#continue-button").disabled = false;
     document.querySelector("button#continue-button").click();
     msg = msg + "Modal cut answer: \n" + answer + "\n";
@@ -99,16 +107,16 @@
       try {
         let question = document.querySelector("#question-text").innerText;
         let answer = findAnswer(question);
-        if (answer === undefined)
-          console.log("no answer found");
-        answerSub = answer
+        if (answer === undefined) console.log("no answer found");
+        answerSub = answer;
 
         if (semiTOGGLE === true) {
           try {
-            await navigator.clipboard.writeText(answerSub)
+            await navigator.clipboard.writeText(answerSub);
           } catch {
             console.log(
-                "if you're on Safari, ignore this error, if not, please report this as a bug at EducationPerfected if something isn't working")
+              "if you're on Safari, ignore this error, if not, please report this as a bug at EducationPerfected if something isn't working"
+            );
           }
         }
 
@@ -116,8 +124,10 @@
           await submitAnswer(answerSub);
         }
 
-        if (document.querySelector("td#correct-answer-field") != null &&
-            document.querySelector("#question-field") != null) {
+        if (
+          document.querySelector("td#correct-answer-field") != null &&
+          document.querySelector("#question-field") != null
+        ) {
           await correctAnswer(question);
           console.log(msg);
           msg = "";
@@ -139,15 +149,17 @@
   }
 
   document.addEventListener("keydown", async (event) => {
-    if ((event.altKey && event.key.toLowerCase() === "r") ||
-        (event.key.toLowerCase() === "®")) {
+    if (
+      (event.altKey && event.key.toLowerCase() === "r") ||
+      event.key.toLowerCase() === "®"
+    ) {
       mergeLists(wordList("targetLanguage"), wordList("baseLanguage"));
       console.log(fullDict, cutDict);
       alert("Word List Refreshed");
-    }
-
-    else if ((event.altKey && event.key.toLowerCase() === "a") ||
-             (event.key.toLowerCase() === "å")) {
+    } else if (
+      (event.altKey && event.key.toLowerCase() === "a") ||
+      event.key.toLowerCase() === "å"
+    ) {
       if (semiTOGGLE === false) {
         autoTOGGLE = false;
         semiTOGGLE = true;
@@ -159,17 +171,20 @@
         alert("Stopping Semi-Auto-Answer");
         semiTOGGLE = false;
       }
-    }
-
-    else if ((event.metaKey && semiTOGGLE === true) ||
-             (event.ctrlKey && semiTOGGLE === true)) {
+    } else if (
+      (event.metaKey && semiTOGGLE === true) ||
+      (event.ctrlKey && semiTOGGLE === true)
+    ) {
       navigator.clipboard.writeText(answerSub).then(
-          function(arr) {},
-          function(arr) { console.log(`Failed to Copy Answer: ${arr}`) });
-    }
-
-    else if ((event.altKey && event.key.toLowerCase() === "s") ||
-             (event.key.toLowerCase() === "ß")) {
+        function (arr) {},
+        function (arr) {
+          console.log(`Failed to Copy Answer: ${arr}`);
+        }
+      );
+    } else if (
+      (event.altKey && event.key.toLowerCase() === "s") ||
+      event.key.toLowerCase() === "ß"
+    ) {
       if (autoTOGGLE === false) {
         semiTOGGLE = false;
         autoTOGGLE = true;
